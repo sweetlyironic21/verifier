5df28337d1e4743f3d0a8910adc9b8f84dc2762a
Sanjil <seanwhitelive@gmail.com>
2023-02-28 02:23:02 +0545

    ok

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 import requests
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+import csv
+
 # Create your views here.
 
 def check_single_mail(email):
@@ -9,11 +15,43 @@ def check_single_mail(email):
     }
     res=requests.post(url=url,json=payload)
     return res.json()
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def bulk_validate(req):
+    if req.method == "POST":
+        emails=req.data["emails"]
+        res=requests.post("http://103.134.217.133:8080/v0/bulk",json={
+                    "input_type": "array",            
+                    "input": emails
+        })
+        print(res.text)
+        data = res.json()
+        return JsonResponse({
+            "job_id":data["job_id"]
+        })
+
+def get_job_status(req,job_id):
+    res=requests.get(f"http://103.134.217.133:8080/v0/bulk/{job_id}")
+    return JsonResponse(res.json())
+
+def get_job_results(req,job_id):
+    res=requests.get(f"http://103.134.217.133:8080/v0/bulk/{job_id}/results?format=csv")
+
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="filename.csv"'},
+    )
+    response.write(res.content)
+    return response
 def home(request):
     if request.method=="POST":
         email=request.POST["email"]
         print(email)
+
         response=check_single_mail(email)
+
         print(response)
         return render(request,"home.html",context={
             "data":response,
