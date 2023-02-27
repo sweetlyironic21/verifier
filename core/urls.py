182dcaecd216d423e49daa0027b28402ed245787
Sujan <sujancode@gmail.com>
2023-02-27 15:10:22 -0500

    CHanges

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home,bulk_validate,get_job_status,get_job_results
 
 urlpatterns=[
-    path("",home)
+    path("",home),
+    path("v0/bulk",bulk_validate),
+    path("v0/bulk/<int:job_id>",get_job_status),
+    path("v0/bulk/<int:job_id>/result",get_job_results)
+
 ]
